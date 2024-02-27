27f1ad08b0917ea0e7a2b2c4e68aa9ab0b20eda2
parhamkhanmohammadi <31302433+parhamkhanmohammadi@users.noreply.github.com>
2024-02-27 12:25:28 +0330

    Create Embed.js

---
+++ Src/Credentials/Embed.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+    embed : {
+    "logo" : "https://cdn.discordapp.com/attachments/1060182216354824233/1099725510910681249/1682245463485.png",
+    "logom" : "https://cdn.discordapp.com/attachments/1060182216354824233/1099731220184698982/discord-avatar-256-R1FF1.gif",
+    "color" : "062953",
+    "name" : "RADAR",
+    "link" : "https://discord.gg/syU7CGjwJU"
+    }
+}
